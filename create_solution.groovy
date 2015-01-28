a44d05a574ac55762352b4588f8a0378695333a2
Martin Walsh <walshmartinpaul@gmail.com>
2015-01-28 22:04:33 +1100

    Add groovy solution generation script

---
+++ create_solution.groovy
@@ -0,0 +1,26 @@
+#!/usr/bin/env groovy
+
+def srcSlashGroovy = new File('src/groovy')
+
+if (!srcSlashGroovy.exists() && !srcSlashGroovy.isDirectory()) {
+    println "Call from project root."
+    return 1
+}
+
+def template = """
+title = { 'Problem : ' }
+
+description = {
+    \"\"\"
+
+\"\"\"
+}
+
+solve = {
+    def result
+
+    return result
+}
+"""
+
+new File(srcSlashGroovy, "${args[0]}.groovy").write(template)
