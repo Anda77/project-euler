b30e6fe91bd2f90953328891c81b72103cd7fee1
Martin Walsh <walshmartinpaul@gmail.com>
2015-01-29 20:24:58 +1100

    Better generation script

--- create_solution.groovy
+++ create_solution.groovy
@@ -6,9 +6,11 @@ if (!srcSlashGroovy.exists() && !srcSlashGroovy.isDirectory()) {
     println "Call from project root."
     return 1
 }
-
+def title = "${args[0][0]}${args[0][1..-1].split('_').join(': ')}"
+title = title.replaceAll(/\B[A-Z]/) { ' ' + it }
+title = title.replaceAll(/Problem(\d+)/, 'Problem $1')
 def template = """
-title = { 'Problem : ' }
+title = { '${title}' }
 
 description = {
     \"\"\"
